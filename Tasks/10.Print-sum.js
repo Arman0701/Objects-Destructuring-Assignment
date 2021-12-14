a4fb00b65ef78278774f00545eb7e817f7655b5d
Arman773 <tadevosyan889@gmail.com>
2021-12-15 01:37:04 +0400

    feat: added new task

---
+++ Tasks/10.Print-sum.js
@@ -0,0 +1,24 @@
+'use strict'
+
+// Create a function that takes infinite count of elements, operator and prints their sum. 
+// If there's no perator, then default should be +
+
+function printSum(operator, ...values){
+    console.log(values);
+    if (operator === '*'){
+        return values.reduce((a,b)=>{return a*b});
+    }
+    else if (operator === '-'){
+        return values.reduce((a,b)=>{return a-b});
+    }
+    else if (operator === '**'){
+        return values.reduce((a,b)=>{return Math.pow(a,b)});
+    } else {
+        return values.reduce((a,b)=>{return a+b}, operator);
+    }
+}
+
+console.log(printSum("*", 1, 2, 3)); // 9
+console.log(printSum(1, 2, 3, 4, 5)); // 15
+console.log(printSum("-", 1, 2, 3, 6, 7)); // -17
+console.log(printSum("**", 2, 3, 2)); // 64
